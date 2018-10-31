951a658bf8d87369751644b244f2189772b3581e
xiaxiangfeng <xiangfeng_xia@suhui.com>
2018-10-31 21:24:01 +0800

    使用class重构

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # 倒计时插件
 
 > 倒计时插件
+
+## 使用方式
+
+``` js
+import { clock } from './clock'
+// 开始
+const clockObj = clock('df_clock', this.props.time).start()
+// 停止
+clockObj.stop()
+``` 
--- index.js
+++ index.js
@@ -1,42 +1,99 @@
-export const clock = function () {
-    var target_date = new Date('2018/01/15 00:00:00').getTime();; // set the countdown date
-    var days,
-        hours,
-        minutes,
-        seconds; // variables for time units
+class Clock {
 
-    var countdown = document.getElementById("tiles"); // get tag element
+  constructor(id, time = '2018/01/01 00:00:00', visibleDays) {
 
-    getCountdown();
+    this.target_date = new Date(time).getTime()
 
-    setInterval(function () {
-        getCountdown();
+    this.countdown = document.getElementById(id)
+
+    this.visibleDays = !!visibleDays
+  }
+
+  visibleDays = false
+
+  target_date
+
+  days
+
+  hours
+
+  minutes
+
+  seconds
+
+  countdown
+
+  timer
+
+  getCountdown () {
+
+    const current_date = new Date().getTime();
+    let seconds_left = (this.target_date - current_date) / 1000;
+
+    this.days = this.pad(parseInt(seconds_left / 86400))
+
+    seconds_left = seconds_left % 86400
+
+    this.hours = this.pad(parseInt(seconds_left / 3600))
+
+    seconds_left = seconds_left % 3600;
+
+    this.minutes = this.pad(parseInt(seconds_left / 60))
+
+    this.seconds = this.pad(parseInt(seconds_left % 60));
+
+    if (this.days === '00' && this.hours === '00' && this.minutes === '00' && this.seconds === '00') {
+      this.stop()
+    }
+
+    this.countdown.innerHTML = this.daysStr + this.hoursStr + this.minutesStr + this.secondsStr
+  }
+
+  get daysStr () {
+    if (!this.visibleDays) return ''
+    return "<span>" + this.days + "</span>"
+  }
+
+  get hoursStr () {
+    return "<span>" + this.hours + "</span>"
+  }
+
+  get minutesStr () {
+    return "<span>" + this.minutes + "</span>"
+  }
+
+  get secondsStr () {
+    return "<span>" + this.seconds + "</span>"
+  }
+
+  pad (n) {
+
+    if (n <= 0) return '00'
+
+    return (n < 10
+      ? '0'
+      : '') + n;
+  }
+
+  start () {
+    this.stop()
+    this.getCountdown()
+    const contxt = this
+    this.timer = setInterval(function () {
+      contxt.getCountdown();
     }, 1000);
 
-    function getCountdown() {
+    return this
+  }
 
-        // find the amount of "seconds" between now and target
-        var current_date = new Date().getTime();
-        var seconds_left = (target_date - current_date) / 1000;
+  stop () {
+    clearInterval(this.timer)
+  }
 
-        days = pad(parseInt(seconds_left / 86400));
-        seconds_left = seconds_left % 86400;
-
-        hours = pad(parseInt(seconds_left / 3600));
-        seconds_left = seconds_left % 3600;
-
-        minutes = pad(parseInt(seconds_left / 60));
-        seconds = pad(parseInt(seconds_left % 60));
-
-        // format countdown string + set tag value
-        countdown.innerHTML = "<span>" + days + "</span><span>" + hours + "</span><span>" + minutes + "</span><span>" + seconds + "</span>";
-    }
-
-    function pad(n) {
-        if(n<= 0) return '00'
-        
-        return (n < 10
-            ? '0'
-            : '') + n;
-    }
 }
+
+export const clock = (id, time, visibleDays) => {
+  return new Clock(id, time, visibleDays)
+}
+
+export default Clock
